3538eca6c3d03d88c7f1831a50639b6b97e3dc9d
Ndumiso <mvelasetheone@gmail.com>
2024-10-28 18:24:03 +0200

    Create CheckInActivity.kt

---
+++ CheckInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.medappfnb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CheckInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_in)
+
+        // Implement patient check-in logic
+    }
+}
