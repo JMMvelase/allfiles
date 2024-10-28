42b6765813f1d1c6d881b37b5ede67d8916503a3
Ndumiso <mvelasetheone@gmail.com>
2024-10-28 18:23:47 +0200

    Create LocationActivity.kt

---
+++ LocationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.medappfnb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+
+        // Display clinic location
+    }
+}
