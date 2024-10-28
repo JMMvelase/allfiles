583648794ba6096ae91e3cf6a9e41aa7bb6b31d1
Ndumiso <mvelasetheone@gmail.com>
2024-10-28 18:22:54 +0200

    Create PatientDetailsActivity.kt

---
+++ PatientDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.medappfnb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PatientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_details)
+
+        // Display patient details
+    }
+}
