aa75578e7cdc153077e84658a6b24c5701452c58
Ndumiso <mvelasetheone@gmail.com>
2024-10-28 18:23:16 +0200

    Create MedicationActivity.kt

---
+++ MedicationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.medappfnb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MedicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medication)
+
+        // Display medication information
+    }
+}
